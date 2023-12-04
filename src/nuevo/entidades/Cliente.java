3892bd1df53025cac9df212bdf253b3d7abacbe4
Rayito391 <ryangpe153@gmail.com>
2023-12-03 22:05:32 -0700

    Listo pa que hagan su parte

--- src/nuevo/entidades/Cliente.java
+++ src/nuevo/entidades/Cliente.java
@@ -1,7 +1,7 @@
 package nuevo.entidades;
 
-import java.util.UUID;
-
+import java.util.UUID;//UUID es para una id(identificador unico)
+//record es una clase que ya tiene incluido los getters, toString y el constructor de los atributos
 public record Cliente(UUID id, String nombre, String direccion) {
     public Cliente(String nombre, String direccion) {
         this(UUID.randomUUID(), nombre, direccion);
--- src/nuevo/entidades/Transaccion.java
+++ src/nuevo/entidades/Transaccion.java
@@ -1,12 +1,12 @@
 package nuevo.entidades;
 
-import java.time.LocalDate;
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDate; // es para la fecha
+import java.util.Optional;  // es para un optional(si o no), para evitar usar nulos
+import java.util.UUID;      // es para identificador unico
 
 public record Transaccion(UUID id, LocalDate fecha, String tipo, Double monto, Optional<Cuenta> destino, String banco) {
     public Transaccion(String tipo, Double monto, Cuenta destino, String banco) {
-        this(UUID.randomUUID(), LocalDate.now(), tipo, monto, Optional.of(destino), banco);
+        this(UUID.randomUUID(), LocalDate.now(), tipo, monto, Optional.of(destino), banco); // hace referencia a clase superior con el this
     }
 
     public Transaccion(String tipo, Double monto, String banco) {
